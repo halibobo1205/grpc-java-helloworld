5efd1685b1e781399a567dad5a03d5df4578f2b2
halibobo1205 <halibobo1205@gmail.com>
2023-01-31 16:58:11 +0800

    use nginx

--- src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -66,7 +66,7 @@ public class HelloWorldClient {
   public static void main(String[] args) throws Exception {
     String user = "world";
     // Access a service running on the local machine on port 50051
-    String target = "localhost:50051";
+    String target = "grpc.halibobo.cn";
     // Allow passing in the user and target strings as command line arguments
     if (args.length > 0) {
       if ("--help".equals(args[0])) {
