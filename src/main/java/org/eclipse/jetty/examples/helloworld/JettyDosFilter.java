2617c9d7e5eaf931d4ea9ef9437343d370c5dbf6
halibobo1205 <halibobo1205@gmail.com>
2023-08-22 15:34:28 +0800

    correct dosFilter

--- src/main/java/org/eclipse/jetty/examples/helloworld/JettyDosFilter.java
+++ src/main/java/org/eclipse/jetty/examples/helloworld/JettyDosFilter.java
@@ -20,18 +20,18 @@ public class JettyDosFilter extends CloseableDoSFilter {
         super.init(filterConfig);
 
         setEnabled(true);
-
+        setMaxRequestMs(3000);      // Default is 30000. Maximum time, in ms, to allow the request to run. Requests that exceed this are rejected.
         setMaxRequestsPerSec(1);    // Default is 25. Maximum number of requests from a connection per second. Requests in excess of this are first delayed, then throttled.
         setDelayMs(3000);           // Delay imposed on all requests over the rate limit, before they are considered at all
-        //setMaxWaitMs(50);         // Length of time, in ms, to blocking wait for the throttle semaphore. Default is 50 ms.
-        //setThrottledRequests(5);  // Number of requests over the rate limit able to be considered at once. Default is 5.
-        //setThrottleMs(3000L);     //Length of time, in ms, to async wait for semaphore. Default is 30000L.
-        //setMaxRequestsPerSec(3000L);    //Length of time, in ms, to allow the request to run. Default is 30000L.
-        //setMaxIdleTrackerMs(3000L); //Length of time, in ms, to keep track of request rates for a connection, before deciding that the user has gone away, and discarding it. Default is 30000L.
+        setMaxWaitMs(50);         // Length of time, in ms, to blocking wait for the throttle semaphore. Default is 50 ms.
+        setThrottledRequests(5);  // Number of requests over the rate limit able to be considered at once. Default is 5.
+        setThrottleMs(3000L);     //Length of time, in ms, to async wait for semaphore. Default is 30000L.
+        setMaxRequestsPerSec(3000); //Default is 0. Maximum number of requests from a connection per second. Requests in excess of this are first delayed, then throttled.
+        setMaxIdleTrackerMs(3000L); //Length of time, in ms, to keep track of request rates for a connection, before deciding that the user has gone away, and discarding it. Default is 30000L.
         setInsertHeaders(true);     //Defaults to true. Insert the DoSFilter headers into the response.
         setTrackSessions(true);     //Defaults to true. Usage rate is tracked by session if a session exists.
-        //setRemotePort(false);     //Defaults to false.  If true and session tracking is not used, then rate is tracked by IP and port (effectively connection).
-        //setWhitelist(new String("0:0:0:0:0:0:0:1,192.169.0.1"));    //A comma-separated list of IP addresses that will not be rate limited.
+        setRemotePort(false);     //Defaults to false.  If true and session tracking is not used, then rate is tracked by IP and port (effectively connection).
+        //setWhitelist("0:0:0:0:0:0:0:1,192.169.0.1");    //A comma-separated list of IP addresses that will not be rate limited.
     }
 
     @Override
