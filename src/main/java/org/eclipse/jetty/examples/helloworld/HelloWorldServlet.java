91258f6a390e1363f341c4e3a454b6dd8ba4c61a
halibobo1205 <halibobo1205@gmail.com>
2023-08-22 15:35:27 +0800

    servlet opt

--- src/main/java/org/eclipse/jetty/examples/helloworld/HelloWorldServlet.java
+++ src/main/java/org/eclipse/jetty/examples/helloworld/HelloWorldServlet.java
@@ -17,17 +17,23 @@
 package org.eclipse.jetty.examples.helloworld;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.http.HttpMethod;
 import org.eclipse.jetty.http.MimeTypes;
+import org.eclipse.jetty.util.MultiMap;
+import org.eclipse.jetty.util.StringUtil;
+import org.eclipse.jetty.util.UrlEncoded;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Server that manages startup/shutdown of a {@code Greeter} server.
@@ -47,8 +53,16 @@ public class HelloWorldServlet extends HttpServlet {
     try {
       HelloRequest req = buildRequest(request);
       fillResponse(req, response);
-    } catch (Exception ignored) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    } catch (Exception e) {
+      try {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("error", e.getMessage());
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setContentType(MimeTypes.Type.APPLICATION_JSON.asString());
+        response.getWriter().println(jsonObject);
+      } catch (IOException ex) {
+        logger.error("error: {}", ex.getMessage());
+      }
     }
   }
 
@@ -57,15 +71,29 @@ public class HelloWorldServlet extends HttpServlet {
       return HelloRequest.newBuilder().setName(request.getParameter("name")).build();
     }
     if (HttpMethod.POST.is(request.getMethod())) {
+      Stream.of(MimeTypes.Type.FORM_ENCODED,
+              MimeTypes.Type.APPLICATION_JSON, MimeTypes.Type.TEXT_JSON,
+              MimeTypes.Type.APPLICATION_JSON_8859_1, MimeTypes.Type.APPLICATION_JSON_UTF_8,
+              MimeTypes.Type.TEXT_JSON_8859_1, MimeTypes.Type.TEXT_JSON_UTF_8)
+          .filter(type -> type.is(request.getContentType()))
+          .findFirst()
+          .orElseThrow(() -> new UnsupportedOperationException("Unsupported content type"));
+      String requestData = request.getReader().lines().collect(Collectors.joining());
       if (MimeTypes.Type.FORM_ENCODED.is(request.getContentType())) {
-        return HelloRequest.newBuilder().setName(request.getParameter("name")).build();
+        requestData = getJsonString(requestData);
       }
-      if (MimeTypes.Type.APPLICATION_JSON.is(request.getContentType())) {
-        String requestData = request.getReader().lines().collect(Collectors.joining());
-        return new Gson().fromJson(requestData, HelloRequest.class);
-      }
+      return GsonUtil.toMessage(HelloRequest.class, requestData);
     }
-     throw new UnsupportedOperationException();
+    throw new UnsupportedOperationException("Unsupported method");
+  }
+
+  public static String getJsonString(String str) {
+    if (StringUtil.isEmpty(str)) {
+      return new Gson().toJson(Collections.emptyMap());
+    }
+    MultiMap<String> params = new MultiMap<>();
+    UrlEncoded.decodeUtf8To(str, params);
+    return new Gson().toJson(params);
   }
 
   private void fillResponse(HelloRequest req, HttpServletResponse response) throws IOException {
